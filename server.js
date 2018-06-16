7536a03d4cde7ae86cd5dcf85380a82fe868cf8b
Leon Jalfon <leonj@sela.co.il>
2018-06-16 13:36:00 +0300

    fix resources issue

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var app = express();
 var port = 3000;
 var fs = require('fs');
 
+app.use(express.static("."));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res){
 	fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
   });
