b75304743cad64b753bf1bb509544236914b76e6
Leon Jalfon <leonj@sela.co.il>
2018-09-02 16:09:36 +0000

    Update services-controller.js

--- services-controller.js
+++ services-controller.js
@@ -3,22 +3,22 @@ angular.module('services', [])
     
     $scope.calculate = function() {
 
-        $http.get("http://localhost:3001/sum/"+$scope.number1+"/"+$scope.number2)
+        $http.get("http://HOST_IP:3001/sum/"+$scope.number1+"/"+$scope.number2)
 		.then(function(response) {
             $scope.sum = response.data;
         });
 	
-        $http.get("http://localhost:3002/subtraction/"+$scope.number1+"/"+$scope.number2)
+        $http.get("http://HOST_IP:3002/subtraction/"+$scope.number1+"/"+$scope.number2)
 		.then(function(response) {
             $scope.subtraction = response.data;
         });
 		
-	    $http.get("http://localhost:3003/multiplication/"+$scope.number1+"/"+$scope.number2)
+	    $http.get("http://HOST_IP:3003/multiplication/"+$scope.number1+"/"+$scope.number2)
 		.then(function(response) {
             $scope.multiplication = response.data;
         });
 		
-	    $http.get("http://localhost:3004/division/"+$scope.number1+"/"+$scope.number2)
+	    $http.get("http://HOST_IP:3004/division/"+$scope.number1+"/"+$scope.number2)
 		.then(function(response) {
             $scope.division = response.data;
         });
